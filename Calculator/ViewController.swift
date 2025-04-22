72c957521e85e5d22580c634c72c205666e6c0fc
Adilet <bbddbbd2003@gmail.com>
2025-04-22 17:48:43 +0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Adilet Kenesbekov on 22.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Adilet Kenesbekov on 22.04.2025.
+//
+
+import Testing
+@testable import Calculator
+
+struct CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
